b9a34868e80e2f44865a453ea1539f14a08ee65d
DESKTOP-L3B48PA\khadija.rauf <khadijarauf613@gmail.com>
2023-08-23 15:09:47 +0500

    Lecture02- Spring Data JPA, REST, and Liquibase.

---
+++ Lecture02/src/main/java/com/redmath/training/core/NewsController.java
@@ -0,0 +1,69 @@
+package com.redmath.training.core;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import ch.qos.logback.core.pattern.parser.OptionTokenizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/news")
+public class NewsController {
+
+    private final NewsService service;
+
+    public NewsController(NewsService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity <List<News>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<News> findById(@PathVariable("id") Long id) {
+        Optional<News> news = service.findById(id);
+        if (news.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(news.get());
+    }
+
+    @PostMapping()
+    public ResponseEntity<News> create(@RequestBody News news) {
+        News created = service.create(news);
+        if (created != null) {
+            return ResponseEntity.ok(created);
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Optional<News> news = service.findById(id);
+        if (news.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        News news1 = news.get();
+        service.delete(news1);
+        return ResponseEntity.noContent().build();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<News> updateNews(@PathVariable Long id, @RequestBody News updatedNews) {
+        News news = service.updateNews(id, updatedNews);
+
+        if (news != null) {
+            return ResponseEntity.ok(news);
+        } else {
+            return ResponseEntity.notFound().build();
+
+        }
+    }
+//
+//    @DeleteMapping("/{id}")
+//    public void delete(Long id) {
+//    }
+}
---
+++ Lecture02/src/main/java/com/redmath/training/core/NewsService.java
@@ -0,0 +1,57 @@
+package com.redmath.training.core;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Optional;
+@Service
+public class NewsService {
+
+    private final NewsRepository repository;
+
+    public NewsService(NewsRepository repository) {
+
+        this.repository = repository;
+    }
+
+    public List<News> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<News> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public News create(News news) {
+        if (news.getId() != null && repository.existsById(news.getId())) {
+            return null;
+        }
+        news.setId(System.currentTimeMillis());
+        news.setReportedAt(LocalDateTime.now());
+        return repository.save(news);
+    }
+    public Optional<Void> delete(News news){
+        if (news.getId() != null && repository.existsById(news.getId())) {
+            repository.delete(news);
+            return Optional.empty();
+        }
+
+        return null;
+    }
+    public News updateNews(Long id, News updatedNews) {
+        Optional<News> optionalNews = repository.findById(id);
+
+        if (optionalNews.isPresent()) {
+            News existingNews = optionalNews.get();
+            // Update the existingNews object with the data from updatedNews
+            existingNews.setTitle(updatedNews.getTitle());
+            existingNews.setDetails(updatedNews.getDetails());
+
+            return repository.save(existingNews);
+        } else {
+            return null;
+        }
+    }
+}
